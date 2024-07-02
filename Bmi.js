4b6ce456942cdce865948514a3f81c3d1b836452
ukpono andem <ukponoadem>
2024-07-02 09:50:24 +0100

    java_script

---
+++ Bmi.js
@@ -0,0 +1,65 @@
+ let weight = 29
+ let height = 29.9
+/*
+ let bmi = weight/(height*height)
+ if (bmi <= 18){
+    console.log("Under weight")
+ }
+  else if (18 >= bmi <= 25){
+    console.log("Normal weight")
+  }
+  else if (25 >= bmi <= 30 ){
+    console.log("overweight")
+  }
+  else if(bmi <= 30){
+    console.log("obesity")
+  }else{
+    console.log("no weight and height found")
+  }
+  
+console.log(Math.round(bmi))
+*/
+
+/*
+ let student_Grade =  ( totalgrades / 400 ) *  100 ;
+ if (student_Grade <= 100 || student_Grade >=90){
+    alert("Grade A")
+ }
+ else if (student_Grade>=80 || student_Grade < 90){
+    alert("Grade B")
+ }
+ else if (student_Grade>=70 || student_Grade < 80){
+    alert("Grade C")
+ }
+ else if (student_Grade >=60 || student_Grade < 70){
+    alert("Grade E")
+ }
+ else if (student_Grade >= 50 || student_Grade < 60 ){
+    alert("Grade F")
+ }else{
+    alert("No grade found")
+ }
+*/
+
+/*
+let totalgrades = parseFloat("80")
+let grade =   ( totalgrades / 400 ) *  100 ;
+function grade(){
+  if (grade <=100 ||grade >=90){
+     alert("Grade A")
+  }
+  else if (grade <=90 || grade >=80){
+     alert("Grade B")
+  }
+}
+
+
+console.log(grade)
+
+*/
+
+
+
+
+
+
